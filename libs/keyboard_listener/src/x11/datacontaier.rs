4fc47616d6f5503083f2a33e4580424ae4cbc481
Денис Котляров <denis2005991@gmail.com>
2025-09-04 13:38:08 +0300

    fix: improve security of x11 data container

--- libs/keyboard_listener/src/x11/datacontaier.rs
+++ libs/keyboard_listener/src/x11/datacontaier.rs
@@ -1,6 +1,16 @@
 use std::ops::Deref;
 use std::ops::DerefMut;
+use std::ptr;
 use std::sync::Mutex;
+
+/// Vulnerable numbers to detect overwriting
+#[cfg(target_pointer_width = "64")]
+const ALTERNATING_MASK: usize = 0xAAAAAAAAAAAAAAAA;
+
+/// Vulnerable numbers to detect overwriting
+#[cfg(target_pointer_width = "32")]
+const ALTERNATING_MASK: usize = 0xAAAAAAAA;
+
 
 /// Since extern can be called by anyone at any time, and I don't want to make `callback`
 /// static... you could do pointer substitution directly from raw_record_callback,
@@ -20,19 +30,21 @@ pub struct ExterDataContainer<T> {
 impl<T> ExterDataContainer<T> {
 	pub fn container(data: T) -> SafeDropExternDataContainer<T> {
 		let data = Box::new(Self {
-			lpadding: usize::MAX,
+			lpadding: ALTERNATING_MASK,
 			data: Mutex::new(Some(data)),
-			rpadding: usize::MAX,
+			rpadding: ALTERNATING_MASK,
 		});
 
 		SafeDropExternDataContainer(data)
 	}
 
 	pub fn check_and_lock<R>(&self, mut next: impl FnMut(&mut T) -> R) -> Option<R> {
-		if self.lpadding == usize::MAX && self.rpadding == usize::MAX {
+		if self.lpadding == ALTERNATING_MASK && self.rpadding == ALTERNATING_MASK {
 			if let Ok(mut guard) = self.data.lock() {
-				if let Some(ldata) = &mut *guard {
-					return Some(next(ldata));
+				if self.lpadding == ALTERNATING_MASK && self.rpadding == ALTERNATING_MASK {
+					if let Some(ldata) = &mut *guard {
+						return Some(next(ldata));
+					}
 				}
 			}
 		}
@@ -71,8 +83,13 @@ impl<T> DerefMut for SafeDropExternDataContainer<T> {
 
 impl<T> Drop for SafeDropExternDataContainer<T> {
 	fn drop(&mut self) {
-		self.lpadding = 0;
-		self.rpadding = 0;
+		// Force the compiler to create this entry even if it thinks it's unnecessary, 
+		// although on Box it may not be necessary.
+		unsafe {
+			ptr::write_volatile(&mut self.lpadding, 0);
+			ptr::write_volatile(&mut self.rpadding, 0);
+		}
+		
 		*(self.0.data.lock().unwrap_or_else(|e| e.into_inner())) = None;
 	}
 }
